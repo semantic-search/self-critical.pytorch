c13d6dab27247cbe89c5794ff0b9a10db74658ba
Akshay Ashok <aks28id@gmail.com>
2020-08-21 16:55:51 +0530

    added kafka

--- api/server.py
+++ api/server.py
@@ -1,11 +1,47 @@
-from fastapi import FastAPI, File, UploadFile
-
 from critical import FeatureExtractor
 
 import torch
 import captioning
 import captioning.utils.misc
 import captioning.models
+
+# imports for env kafka
+from dotenv import load_dotenv
+from kafka import  KafkaProducer
+from kafka import KafkaConsumer
+from json import loads
+import base64
+import json
+import os
+
+load_dotenv()
+
+KAFKA_HOSTNAME = os.getenv("KAFKA_HOSTNAME")
+KAFKA_PORT = os.getenv("KAFKA_PORT")
+
+RECEIVE_TOPIC = 'SELF_CRITICAL'
+SEND_TOPIC_FULL = "IMAGE_RESULTS"
+SEND_TOPIC_TEXT = "TEXT"
+
+print(f"kafka : {KAFKA_HOSTNAME}:{KAFKA_PORT}")
+
+# To receive img data to process
+consumer_self_critical = KafkaConsumer(
+    RECEIVE_TOPIC,
+    bootstrap_servers=[f"{KAFKA_HOSTNAME}:{KAFKA_PORT}"],
+    auto_offset_reset="earliest",
+    enable_auto_commit=True,
+    group_id="my-group",
+    value_deserializer=lambda x: loads(x.decode("utf-8")),
+)
+
+# For Sending processed img data further 
+producer = KafkaProducer(
+    bootstrap_servers=[f"{KAFKA_HOSTNAME}:{KAFKA_PORT}"],
+    value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+)
+
+
 infos = captioning.utils.misc.pickle_load(open('/models/infos_trans12-best.pkl', 'rb'))
 
 feature_extractor = FeatureExtractor()
@@ -20,12 +56,26 @@ def get_captions(img_feature):
     # Return the 5 captions from beam serach with beam size 5
     return model.decode_sequence(model(img_feature.mean(0)[None], img_feature[None], mode='sample', opt={'beam_size':5, 'sample_method':'beam_search', 'sample_n':5})[0])
 
-app = FastAPI()
+for message in consumer_kerasocr:
+    print('xxx--- inside consumer_kerasocr---xxx')
+    print(f"kafka - - : {KAFKA_HOSTNAME}:{KAFKA_PORT}")
 
-@app.post("/caption/")
-def create_upload_file(file: UploadFile = File(...)):
-    fileName = file.filename
+    
+    message = message.value
+    image_id = message['image_id']
+    data = message['data']
 
-    captions = get_captions(feature_extractor(file.file))
+    data = base64.b64decode(data.encode("ascii"))
 
-    return ' '.join(captions)
+    captions = get_captions(feature_extractor(data))
+    captions = ' '.join(captions)
+    response = {
+        'image_id': image_id,
+        'data': captions
+    }
+    
+    # sending full and text res(without cordinates or probability) to kafka
+    producer.send(SEND_TOPIC_FULL, value=response)
+    producer.send(SEND_TOPIC_TEXT, value=response)
+
+    producer.flush()
